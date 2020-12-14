92962c88677d954086f57c53bda9f04bd106b7ae
pamecifras <pamela.cifras@outlook.es>
2020-12-14 12:27:07 -0300

    * Commit avance

--- __manifest__.py
+++ __manifest__.py
@@ -25,6 +25,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/view_pedido.xml',
+        'views/view_boleta.xml',
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode
