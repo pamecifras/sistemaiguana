6fbc098c96a897cb6587bf68c610b19fc56185ec
pamecifras <pamela.cifras@outlook.es>
2020-12-17 22:06:27 -0300

    Commit Avance cambios

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/view_pedido.xml',
-        'views/view_boleta.xml',
         #'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- models/pedido.py
+++ models/pedido.py
@@ -2,32 +2,27 @@
 
 from odoo import models, fields, api
 
+class TipoPago(models.Model):
+    _name = 'pedido.tipo_pago'
+    
+    name = fields.Char(string="Tipo de Pago")
+
 class Pedido(models.Model):
     _name = 'pedido.boleta'
 
     name = fields.Char(string="Nombre Cliente", required=True)
-    fecha = fields.Date()
+    orden = fields.Char(string="Orden", required=True)
+    fecha = fields.Date("Fecha de venta")
     sub_total = fields.Float(default=0)
     descuento = fields.Float(default=0)
     total = fields.Float(default=0)
     
-    detalle_boleta_ids = fields.One2many(
-        'pedido.detalle_boleta', 'boleta_id', String ="Detalle de boleta")
+    tipo_pago_id = fields.Many2one(
+        'pedido.tipo_pago', string = "Tipo de Pago")
     
-class DetalleBoleta(models.Model):
-    _name = 'pedido.detalle_boleta'
-    boleta_id = fields.Many2one('pedido.boleta', String ="Boleta")
-    cantidad = fields.Integer(default=1)
-    precio = fields.Integer()
-    sub_total = fields.Integer(string="Subtotal", compute ="_sub_total")
-    total = fields.Integer(string="Total", compute ="total_")
+
     
-    @api.one
-    def _sub_total(self):
-        self.sub_total= (self.cantidad * self.precio)
-        
-    @api.one
-    def total_(self):
-        self.total= (self.sub_total - ((self.descuento/100) * self.sub_total))
-             
-        
+    
+    
+
+    
