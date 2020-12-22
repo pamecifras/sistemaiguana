9f3512d092856e648334bb12259968aebc5a157c
pamecifras <pamela.cifras@outlook.es>
2020-12-21 22:56:58 -0300

    Commit cambio funcion final

--- models/pedido.py
+++ models/pedido.py
@@ -6,18 +6,18 @@ from odoo import models, fields, api
 class TipoPago(models.Model):
     _name = 'pedido.tipo_pago'
 
-    name = fields.Char(string="Tipo de Pago")
+    name = fields.Char(string = "Tipo de Pago")
 
 
 class Pedido(models.Model):
     _name = 'pedido.boleta'
 
-    name = fields.Char(string="Nombre Cliente", required=True)
+    name = fields.Char(string = "Nombre Cliente", required = True)
     fecha = fields.Date("Fecha de venta")
-    total = fields.Float(default=0)
+    total = fields.Float(default = 0)
 
     tipo_pago_id = fields.Many2one(
-        'pedido.tipo_pago', string="Tipo de Pago")
+        'pedido.tipo_pago', string = "Tipo de Pago")
 
     detalle_boleta_ids = fields.One2many(
         'pedido.detalle_boleta', 'boleta_id')
@@ -26,10 +26,10 @@ class Pedido(models.Model):
 class DetallePedido(models.Model):
     _name = 'pedido.detalle_boleta'
 
-    orden = fields.Char(string="Orden", required=True)
+    orden = fields.Char(string = "Orden", required = True)
     cantidad = fields.Integer()
     precio = fields.Integer()
-    total = fields.Integer(string="Total", Compute="_total")
+    total = fields.Integer(string = "Total", compute = "_total")
     boleta_id = fields.Many2one('pedido.boleta')
 
     @api.one
