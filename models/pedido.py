c1493b75bdddb0aef7f7eb5dea7df286f5eada9d
pamecifras <pamela.cifras@outlook.es>
2020-12-21 21:50:20 -0300

    Commit posible final

--- models/pedido.py
+++ models/pedido.py
@@ -2,27 +2,36 @@
 
 from odoo import models, fields, api
 
+
 class TipoPago(models.Model):
     _name = 'pedido.tipo_pago'
-    
+
     name = fields.Char(string="Tipo de Pago")
+
 
 class Pedido(models.Model):
     _name = 'pedido.boleta'
 
     name = fields.Char(string="Nombre Cliente", required=True)
-    orden = fields.Char(string="Orden", required=True)
     fecha = fields.Date("Fecha de venta")
-    sub_total = fields.Float(default=0)
-    descuento = fields.Float(default=0)
     total = fields.Float(default=0)
-    
+
     tipo_pago_id = fields.Many2one(
-        'pedido.tipo_pago', string = "Tipo de Pago")
-    
+        'pedido.tipo_pago', string="Tipo de Pago")
 
-    
-    
-    
+    detalle_boleta_ids = fields.One2many(
+        'pedido.detalle_boleta', 'boleta_id')
 
-    
+
+class DetallePedido(models.Model):
+    _name = 'pedido.detalle_boleta'
+
+    orden = fields.Char(string="Orden", required=True)
+    cantidad = fields.Integer()
+    precio = fields.Integer()
+    total = fields.Integer(string="Total", Compute="_total")
+    boleta_id = fields.Many2one('pedido.boleta')
+
+    @api.one
+    def _total(self):
+        self.total = (self.cantidad * self.precio)
